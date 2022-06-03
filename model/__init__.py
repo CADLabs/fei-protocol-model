df39c68ae88040f7fe18d55c0f2bdbf03b041125
giutri <giulio.trichilo@gmail.com>
2022-06-03 11:20:19 +0100

    test commit

--- model/__init__.py
+++ model/__init__.py
@@ -7,7 +7,7 @@ from radcad import Model
 
 from model.system_parameters import parameters
 from model.state_variables import initial_state
-from model.state_update_blocks import state_update_blocks
+from model.state_update_blocks import state_update_blocks 
 
 
 # Instantiate a new Model

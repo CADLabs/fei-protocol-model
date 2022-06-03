a19a2a3e047e8abcbaf9ee6245cd8b7d4fac4eec
Benjamin Scholtz <BenSchZA@users.noreply.github.com>
2022-06-03 14:35:12 +0200

    Price processes (#2)
    * Add volatile asset price process
    
    * Add price processes to PSUB
    
    * Fix typo

--- model/state_update_blocks.py
+++ model/state_update_blocks.py
@@ -2,6 +2,9 @@
 cadCAD model State Update Block structure, composed of Policy and State Update Functions
 """
 
+
+import model.parts.price_processes as price_processes
+
 from model.system_parameters import parameters
 from model.utils import update_from_signal
 
@@ -9,8 +12,12 @@ from model.utils import update_from_signal
 state_update_blocks = [
     {
         "description": """
+        Update FEI and PCV asset prices
         """,
         "policies": {},
-        "variables": {},
+        "variables": {
+            "stable_asset_price": price_processes.update_stable_asset_price,
+            "volatile_asset_price": price_processes.update_volatile_asset_price,
+        },
     },
 ]

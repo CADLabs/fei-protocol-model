d52ec2639ac2e55f1f08239ed1d5be99786a60b5
cadlabsgt <giulio@cadlabs.org>
2022-06-06 19:56:10 +0100

    construction of trivial rebalance policy and gauge of results

--- model/parts/pcv_management.py
+++ model/parts/pcv_management.py
@@ -13,11 +13,50 @@ def policy_pcv_rebalancing(params, substep, state_history, previous_state):
     # if bad market conditions volatile asset target is decreased
     
     # MODEL: does this policy need to be called after all these values are calculated?
-    cr = previous_state['collateralization_ratio']
+    # cr = previous_state['collateralization_ratio']
     volatile_asset_price = previous_state['volatile_asset_price']
     stable_asset_price = previous_state['stable_asset_price']
-    total_volatile_asset_pcv_value = previous_state['total_volatile_asset_pcv_value']
-    total_stable_asset_pcv_value = previous_state['total_stable_asset_pcv_value']
+    total_pcv_value = previous_state['total_pcv_value']
+    stable_backing_ratio = previous_state['stable_backing_ratio']
+
+    dt = params["dt"]
+    timestep = previous_state["timestep"]
+    
+    # arbitrarily rebalance every X periods
+    rebalancing_period = 50 * dt
+
+    # the stable backing ratio is what % of PCV value is backed by stable assets
+    # in this two-asset model this is the same thing as the % weight of stables for PCV
+    stable_allocation = stable_backing_ratio
+    volatile_allocation = 1 - stable_backing_ratio
+    
+    current_allocation = {
+        'stable_asset': stable_allocation,
+        'volatile_asset': volatile_allocation,
+    }
+    
+    target_allocation = {
+        'stable_asset': 0.5,
+        'volatile_asset': 0.5,
+    }
+    
+    # how much of each asset to buy/sell after the policy has evaluated
+    stable_asset_target_amount_change = 0.0
+    volatile_asset_target_amount_change = 0.0
+    
+    # condition of being under-backed by stable asset
+    if (current_allocation['stable_asset'] < target_allocation['stable_asset']
+    # and VOLATILITY_CONDITION
+    # and/or WHATEVER_ELSE
+    and timestep % rebalancing_period == 0):
+        
+        stable_allocation_pct_change = target_allocation['stable_asset'] - current_allocation['stable_asset']
+        stable_asset_target_value_change = stable_allocation_pct_change * total_pcv_value
+        stable_asset_target_amount_change = stable_asset_target_value_change / stable_asset_price
+        
+        volatile_allocation_pct_change = target_allocation['volatile_asset'] - current_allocation['volatile_asset']
+        volatile_asset_target_value_change = volatile_allocation_pct_change * total_pcv_value
+        volatile_asset_target_amount_change = volatile_asset_target_value_change / volatile_asset_price
 
     # should use value weights to attempt to answer - based on the current weighting in
     # value for stable and volatile assets, what quantity of each asset: volatile / stable
@@ -31,8 +70,8 @@ def policy_pcv_rebalancing(params, substep, state_history, previous_state):
     
     # NB: these are NOT weights
     return {
-        "stable_asset_pcv_target_change": 0.000,
-        "volatile_asset_pcv_target_change": -0.000,
+        "stable_asset_target_amount_change": stable_asset_target_amount_change,
+        "volatile_asset_target_amount_change": volatile_asset_target_amount_change,
     }
 
 
@@ -43,8 +82,8 @@ def update_total_stable_asset_pcv_amount(params, substep, state_history, previou
     # ASSUMPTION: can directly change the total amount. more realistically
     # needs to be changed at the constituent level
     # DEBUG: placeholder dynamics
-    pct_target = policy_input['stable_asset_pcv_target_change']
-    total_stable_asset_pcv_amount = previous_state['total_stable_asset_pcv_amount'] * (1 + pct_target)
+    target_amount_change = policy_input['stable_asset_target_amount_change']
+    total_stable_asset_pcv_amount = previous_state['total_stable_asset_pcv_amount'] + target_amount_change
     
     return "total_stable_asset_pcv_amount", total_stable_asset_pcv_amount
 
@@ -56,8 +95,8 @@ def update_total_volatile_asset_pcv_amount(params, substep, state_history, previ
     # ASSUMPTION: can directly change the total amount. more realistically
     # needs to be changed at the constituent level
     # DEBUG: placeholder dynamics
-    pct_target = policy_input['volatile_asset_pcv_target_change']
-    total_volatile_asset_pcv_amount = previous_state['total_volatile_asset_pcv_amount'] * (1 + pct_target)
+    target_amount_change = policy_input['volatile_asset_target_amount_change']
+    total_volatile_asset_pcv_amount = previous_state['total_volatile_asset_pcv_amount'] + target_amount_change
     
     return "total_volatile_asset_pcv_amount", total_volatile_asset_pcv_amount
 

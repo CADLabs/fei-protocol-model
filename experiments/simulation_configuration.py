b9d32dc41fa40f6aa54930ab41331f5acb1aca23
BenSchZA <BenSchZA@users.noreply.github.com>
2022-06-07 12:21:28 +0200

    Code formatting

--- experiments/simulation_configuration.py
+++ experiments/simulation_configuration.py
@@ -5,11 +5,11 @@ Simulation configuration such as the number of timesteps and Monte Carlo runs
 from model.constants import days_per_month
 
 
-DELTA_TIME = 1 #days_per_month  # days per timestep
+DELTA_TIME = 1  # days per timestep
 SIMULATION_TIMESTEPS = 400
 # SIMULATION_TIME_MONTHS = 12  # number of months
 # TIMESTEPS = (
 #     days_per_month * SIMULATION_TIME_MONTHS // DELTA_TIME
 # )  # number of simulation timesteps
-TIMESTEPS = ( SIMULATION_TIMESTEPS // DELTA_TIME)
+TIMESTEPS = SIMULATION_TIMESTEPS // DELTA_TIME
 MONTE_CARLO_RUNS = 1  # number of runs

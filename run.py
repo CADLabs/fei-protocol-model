f5e7913ee6f3c520d31c8ecc39ab4b2577b9f87a
BenSchZA <BenSchZA@users.noreply.github.com>
2022-06-14 13:54:32 -0400

    Add launch.json template

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+"""VS Code Python debugger run file
+Run file in the root of project directory for execution of simulation with VS Code Python debugger attached.
+
+Create a VS Code launch.json file from the template and place in .vscode/ directory to configure debugger.
+"""
+
 from experiments.run import run
 
 

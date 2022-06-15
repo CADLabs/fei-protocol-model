2d11698e6aa4e8a09c068d5b811384319409522f
BenSchZA <BenSchZA@users.noreply.github.com>
2022-06-15 14:31:04 -0400

    Final money market updates

--- model/parts/money_markets.py
+++ model/parts/money_markets.py
@@ -3,46 +3,51 @@ from model.constants import wei, blocks_per_year
 
 
 def policy_money_market(params, substep, state_history, previous_state):
-    # Parameters
-    dt = params["dt"]
+    """Money Market Policy
+    The Compound lending market "Compound Jump Rate Model", shared by a number of lending markets including Aave,
+    is used as a proxy for all lending markets in this model.
 
-    # State Variables
-    fei_deposit_money_market: PCVDeposit = previous_state["fei_deposit_money_market"]
-    balance = fei_deposit_money_market.balance
-    borrowed: FEI = previous_state["fei_money_market_borrowed"]
-
-    # TODO Placeholder dynamics: increase borrowed amount until 95% utilization (above kink of 80%)
-    # borrowed amount is added to itself every period - linearly increasing up until the ceiling
-    borrowed = min(balance * 0.95, borrowed + balance * 0.005)
-
-    """Compound Jump Rate Model
     See parameters here:
     * https://etherscan.io/token/0x7713dd9ca933848f6819f38b8352d9a15ea73f67#readContract
     * https://etherscan.io/address/0xfb564da37b41b2f6b6edcc3e56fbf523bd9f2012#readContract
 
     See https://app.aave.com/reserve-overview/?underlyingAsset=0x956f47f50a910163d8bf957cf5846d573e7f87ca&marketName=proto_mainnet
-    for validation.
+    for validation of output.
 
-    # For reference: Compound Standard Interest Rate Model
+    ## Compound Standard Interest Rate Model (for reference)
     ```
     base_rate = ...
     multiplier = ...
     borrowing_interest_rate = multiplier * utilization_rate + base_rate
     ```
     """
+    # Parameters
+    dt = params["dt"]
+    base_rate_per_block = params["base_rate_per_block"]
+    multiplier_per_block = params["multiplier_per_block"]
+    jump_multiplier_per_block = params["jump_multiplier_per_block"]
+    kink = params["money_market_kink"]
+    reserve_factor = params["money_market_reserve_factor"]
+
+    # State Variables
+    fei_deposit_money_market: PCVDeposit = previous_state["fei_deposit_money_market"]
+    balance = fei_deposit_money_market.balance
+    borrowed: FEI = previous_state["fei_money_market_borrowed"]
+
+    # TODO Replace placeholder dynamics
+    # Borrowed amount increases linearly until 95% utilization (above kink of 80%)
+    borrowed = min(balance * 0.95, borrowed + balance * 0.005)
+
+    # Compound Jump Rate Model
     # Calculate utilization rate
     # Assume Compound "reserves" or protocolSeizeShare of zero i.e. no profit taken
     cash = balance - borrowed
     reserves = 0
-    utilization_rate = borrowed / (cash + borrowed - reserves) # == borrowed / balance
+    utilization_rate = borrowed / (cash + borrowed - reserves)  # == borrowed / balance
 
     # Calculate borrowing interest rate
-    # TODO Move to parameters
-    base_rate_per_block = 0
-    multiplier_per_block = 23782343987 / wei
-    jump_multiplier_per_block = 518455098934 / wei
-    kink = 800000000000000000 / wei
-
+    # To be consistent with the smart contract configuration,
+    # per-block parameters are used with the interest rate converted to a yearly rate
     borrowing_interest_rate = (
         (
             multiplier_per_block * min(utilization_rate, kink)
@@ -54,7 +59,6 @@ def policy_money_market(params, substep, state_history, previous_state):
     )
 
     # Calculate supply interest rate
-    reserve_factor = 250000000000000000 / wei
     supply_interest_rate = borrowing_interest_rate * utilization_rate * (1 - reserve_factor)
 
     # State Update

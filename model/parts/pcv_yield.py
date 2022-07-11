d0b5c28611a2522f2a3f824c865b1de598fbde55
vmeylan <valentin.meylan@alumni.epfl.ch>
2022-07-11 15:21:58 +0200

    pcv_yield.py formatting suggestions

--- model/parts/pcv_yield.py
+++ model/parts/pcv_yield.py
@@ -5,9 +5,10 @@ from model.types import (
     APR,
     PCVDeposit,
 )
+from model.system_parameters import Parameters
 
 
-def policy_yield_accrual(params, substep, state_history, previous_state):
+def policy_yield_accrual(params: Parameters, substep, state_history, previous_state) -> dict[str: PCVDeposit]:
     """Yield Accrual Policy
     Accrue simple interest on yield-bearing PCV Deposits.
     """
@@ -24,9 +25,9 @@ def policy_yield_accrual(params, substep, state_history, previous_state):
     fei_price = previous_state["fei_price"]
 
     # State Update
-    stable_deposit_yield_bearing.accrue_yield(dt, stable_asset_price)
-    volatile_deposit_yield_bearing.accrue_yield(dt, volatile_asset_price)
-    fei_deposit_money_market.accrue_yield(dt, fei_price)
+    stable_deposit_yield_bearing.accrue_yield(period_in_days=dt, asset_price=stable_asset_price)
+    volatile_deposit_yield_bearing.accrue_yield(period_in_days=dt, asset_price=volatile_asset_price)
+    fei_deposit_money_market.accrue_yield(period_in_days=dt, asset_price=fei_price)
 
     return {
         "stable_deposit_yield_bearing": stable_deposit_yield_bearing,
@@ -35,7 +36,7 @@ def policy_yield_accrual(params, substep, state_history, previous_state):
     }
 
 
-def policy_withdraw_yield(params, substep, state_history, previous_state):
+def policy_withdraw_yield(params: Parameters, substep, state_history, previous_state) -> dict[str: PCVDeposit]:
     """Withdraw Yield Policy
     Withdraw yield into idle PCV Deposit periodically.
     """
@@ -54,16 +55,18 @@ def policy_withdraw_yield(params, substep, state_history, previous_state):
     volatile_asset_price = previous_state["volatile_asset_price"]
 
     # State Update
+    # todo [readability] 2022-07-11: for readability purposes, can we please name a boolean for this
+    #  timestep % yield_withdrawal_period / dt == 0 ? With an explicit flag name?
     if timestep % yield_withdrawal_period / dt == 0:  # Periodic yield withdrawal
         stable_deposit_yield_bearing.transfer_yield(
-            stable_deposit_idle,
-            stable_deposit_yield_bearing.yield_accrued,
-            stable_asset_price,
+            to=stable_deposit_idle,
+            amount=stable_deposit_yield_bearing.yield_accrued,
+            asset_price=stable_asset_price,
         )
         volatile_deposit_yield_bearing.transfer_yield(
-            volatile_deposit_idle,
-            volatile_deposit_yield_bearing.yield_accrued,
-            volatile_asset_price,
+            to=volatile_deposit_idle,
+            amount=volatile_deposit_yield_bearing.yield_accrued,
+            asset_price=volatile_asset_price,
         )
 
     return {
@@ -74,7 +77,7 @@ def policy_withdraw_yield(params, substep, state_history, previous_state):
     }
 
 
-def policy_reinvest_yield(params, substep, state_history, previous_state):
+def policy_reinvest_yield(params: Parameters, substep, state_history, previous_state) -> dict[str: PCVDeposit]:
     """Reinvest Yield Policy
     Reinvest yield accrued periodically.
     """
@@ -91,16 +94,18 @@ def policy_reinvest_yield(params, substep, state_history, previous_state):
     volatile_asset_price = previous_state["volatile_asset_price"]
 
     # State Update
+    # todo [readability] 2022-07-11: for readability purposes, can we please name a boolean for this
+    #  timestep % yield_withdrawal_period / dt == 0 ? With an explicit flag name?
     if timestep % yield_reinvest_period / dt == 0:  # Periodic yield reinvestment
         stable_deposit_yield_bearing.transfer_yield(
-            stable_deposit_yield_bearing,
-            stable_deposit_yield_bearing.yield_accrued,
-            stable_asset_price,
+            to=stable_deposit_yield_bearing,
+            amount=stable_deposit_yield_bearing.yield_accrued,
+            asset_price=stable_asset_price,
         )
         volatile_deposit_yield_bearing.transfer_yield(
-            volatile_deposit_yield_bearing,
-            volatile_deposit_yield_bearing.yield_accrued,
-            volatile_asset_price,
+            to=volatile_deposit_yield_bearing,
+            amount=volatile_deposit_yield_bearing.yield_accrued,
+            asset_price=volatile_asset_price,
         )
 
     return {
